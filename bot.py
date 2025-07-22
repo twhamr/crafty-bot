753c489937022dbd4aeeafc73e630fab5d6442c6
tywahamr <tywahamr@iu.edu>
2025-07-22 11:41:31 -0400

    Implemented ConfigHandler into bot.py and setup first Crafty Controller command

--- bot.py
+++ bot.py
@@ -1,9 +1,19 @@
-import json
 import discord
 from discord.ext import commands
 from discord import app_commands
 
-GUILD_ID = discord.Object(id=1187561911110336522)
+from app.main.handlers.log_handler import LogHandler
+from app.main.handlers.config_handler import ConfigHandler
+from app.main.classes.api.servers import ServerRequests
+
+config = ConfigHandler()
+logger = LogHandler()
+
+server = ServerRequests()
+
+discord_info = config.read_discord()
+GUILD_ID = discord.Object(id=discord_info['guild_id'])
+BOT_TOKEN = discord_info['bot_token']
 
 class Client(commands.Bot):
     async def on_ready(self):
@@ -16,21 +26,18 @@ class Client(commands.Bot):
         print(f"Logged on as {self.user}!")
 
 
-with open(file="./bot_token.json", mode="r") as file:
-    data = json.load(file)
-bot_token = data['token']
-
 intents = discord.Intents.default()
 intents.message_content = True
 client = Client(command_prefix="!", intents=intents)
 
 
-@client.tree.command(name="hello", description="say Hello!", guild=GUILD_ID)
+@client.tree.command(name="getallservers", description="Get all server instances from Crafty Controller", guild=GUILD_ID)
 async def sayHello(interaction: discord.Interaction):
-    await interaction.response.send_message("Hello!")
+    all_servers = server.get_all_servers()
+    await interaction.response.send_message(content=all_servers)
 
 @client.tree.command(name="printer", description="I will print whatever you give me!", guild=GUILD_ID)
 async def printer(interaction: discord.Interaction, printer: str):
     await interaction.response.send_message(content=printer)
 
-client.run(bot_token)
+client.run(BOT_TOKEN)
