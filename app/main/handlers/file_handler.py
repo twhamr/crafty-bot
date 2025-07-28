f5b3cf3f60526e3ecd867af64f0f12a5abda26d9
Tyler Hamrick <tylerwhamrick2003@gmail.com>
2025-07-28 23:07:18 +0000

    Removed LogHandler from FileHandler

--- app/main/handlers/file_handler.py
+++ app/main/handlers/file_handler.py
@@ -1,15 +1,9 @@
 # ------ Libraries ------
 import os
 
-from app.main.handlers.log_handler import LogHandler
-
 
 # ------ Handler: Files ------
 class FileHandler:
-    def __init__(self) -> None:
-        self.logger = LogHandler()
-    
-
     def check_directory(self, dir: str) -> bool:
         """
         Check if a directory exists, creates it if not
@@ -25,13 +19,13 @@ class FileHandler:
             Success/Failure
         """
         if os.path.exists(path=dir):
-            self.logger.create_log(category="system", message=f"INFO: directory [{dir}] already exists")
+            
             return True
         else:
             try:
                 os.makedirs(name=dir)
-                self.logger.create_log(category="system", message=f"INFO: successfully created directory [{dir}]")
+                
                 return True
-            except Exception as e:
-                self.logger.create_log(category="system", message=f"ERROR: [{e}] | failed to create directory [{dir}]")
+            except:
+                
                 return False
